4fbb0bd71150e4f9b2ea7e8fe1a485bae669f46a
Tyler Hawkins <thawkin3@byu.net>
2022-09-29 22:12:48 -0600

    feat: keeps the build directories under version control

---
+++ react-16-app/build/precache-manifest.e8ca8c529c7b9ed688e466753e22b4ef.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cceea0be9f6901ba8c2448f34823cf38",
+    "url": "/react-16-app/build/index.html"
+  },
+  {
+    "revision": "efb23e40bf2805d4f58b",
+    "url": "/react-16-app/build/static/css/main.ffb74bf2.chunk.css"
+  },
+  {
+    "revision": "0fcab4237230ff05882d",
+    "url": "/react-16-app/build/static/js/2.07865e64.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/react-16-app/build/static/js/2.07865e64.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad9e7185a595c68bfe84",
+    "url": "/react-16-app/build/static/js/3.bbb7b1f9.chunk.js"
+  },
+  {
+    "revision": "efb23e40bf2805d4f58b",
+    "url": "/react-16-app/build/static/js/main.eb9747b1.chunk.js"
+  },
+  {
+    "revision": "4652ea60423029b7750a",
+    "url": "/react-16-app/build/static/js/runtime-main.83168e14.js"
+  }
+]);
